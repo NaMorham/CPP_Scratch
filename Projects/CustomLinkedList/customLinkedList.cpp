70fc7ac0d85ea50527df64ee2c10a9f203ddfd5b
NaMorham <namorham@gmail.com>
2024-04-23 22:55:03 +1000

    CPPST-2: Initial project

---
+++ Projects/CustomLinkedList/customLinkedList.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "customLinkedListVer.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Version: " << CustomLinkedList_VersionFull << std::endl << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+//------------------------------------------------------------------------
